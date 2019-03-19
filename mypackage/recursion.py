a663010d82ce153b9e672ae7ae717c4fd1d84521
Samuel Leeuw <samuelleeuw@gmail.com>
2019-03-19 17:12:54 +0200

    Added new functions

--- mypackage/recursion.py
+++ mypackage/recursion.py
@@ -1,1 +1,32 @@
+def sum_array(array):
+    '''Return sum of all items in array'''
+    if len(numList) == 1:
+        return numList[0]
+    else:
+        return numList[0] + listsum(numList[1:])
 
+def fibonacci(n):
+
+    '''Return nth term in fibonacci sequence'''
+    if n==1:
+        return 0
+    elif n==2:
+        return 1
+    else:
+        return fibonacci(n-1)+fibonacci(n-2)
+
+def factorial(n):
+
+    '''Return n!'''
+    if n==0:
+        return 1
+    else:
+        return n*factorial(n-1)
+
+def reverse(word):
+
+    '''Return word in reverse'''
+    if len(word)==0:
+        return word
+    else:
+        return reverse(word[1:]) + word[0]        
